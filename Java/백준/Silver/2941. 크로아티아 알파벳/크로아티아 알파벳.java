be407cc676d53be023ae19610f078bc5d502dff0
jungmyung-lee <jm0541@naver.com>
2025-10-26 21:50:14 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 164 ms, Memory: 17616 KB -BaekjoonHub

---
+++ Java/백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String[] str = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=","z="};
+        
+        for (int i=0; i<str.length; i++){
+            if (s.contains(str[i])) s = s.replace(str[i], "!");
+            
+        }
+        
+        System.out.print(s.length());
+        
+        
+        
+        
+    }
+}
