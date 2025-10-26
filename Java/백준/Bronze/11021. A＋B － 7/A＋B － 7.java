f4fa645963ebb51842c4870a8c58aca4a19e9a28
jungmyunglee <jm0541@naver.com>
2025-10-26 23:48:48 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 16020 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,27 @@
+import java.util.*; 
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i=1; i<=n; i++){
+            st = new StringTokenizer(br.readLine());
+            bw.write("Case #" + i + ": " + (Integer.parseInt(st.nextToken()) +Integer.parseInt(st.nextToken()) ) + "\n");
+            
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+        
+        
+        
+    }
+
+}
