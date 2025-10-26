c92ba8ad8714d4ccc7420c3cea70d9915d532cb9
jungmyung-lee <jm0541@naver.com>
2025-10-26 22:35:29 +0900

    [Bronze III] Title: 공 넣기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+box = [0] * N
+
+for _ in range(M):
+    i,j,k = map(int, input().split())
+    for index in range(i,j+1):
+        box[index-1] = k
+
+for i in range(N):
+    print(box[i], end=" ")
